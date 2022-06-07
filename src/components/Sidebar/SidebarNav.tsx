d33a369460a19cbf4daf3ea7928a87187c459cd1
gabriebarreto <gabriel.devloper>
2022-06-07 17:43:22 -0300

    feat: permissão de listagem de usuarios.

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -3,26 +3,29 @@ import NavLink from "./NavLink";
 import { RiContactsLine, RiDashboardLine } from "react-icons/ri";
 import { BsCollection } from "react-icons/bs";
 import { VStack } from "@chakra-ui/react";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function SidebarNav() {
+  const { user, logout } = useContext(AuthContext);
+
+
   return (
     <VStack spacing="12" alignItems="flex-start">
-      <NavSection title={"GERAL"}>
-        <NavLink href={"/dashboard"} icon={RiDashboardLine}>
-          Dashboard
-        </NavLink>
-        <NavLink href={"/usuarios"} icon={RiContactsLine}>
-          Usuários
-        </NavLink>
-      </NavSection>
-
+      {user?.role.name === "ADMIN" && (
+        <NavSection title={"GERAL"}>
+          <NavLink href={"/dashboard"} icon={RiDashboardLine}>
+            Dashboard
+          </NavLink>
+          <NavLink href={"/usuarios"} icon={RiContactsLine}>
+            Usuários
+          </NavLink>
+        </NavSection>
+      )}
       <NavSection title={"CHAT"}>
         <NavLink href={"/chat"} icon={BsCollection}>
           Chat
         </NavLink>
-        {/* <NavLink href={"/tsnr/recolhimentos"} icon={BsCollection}>
-          Recolhimento
-        </NavLink> */}
       </NavSection>
     </VStack>
   );
